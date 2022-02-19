8925c5f147def1df0527bd6df2706f0e660243e4
PLT\evasquez <eli.vasquez@poly.com>
2022-02-19 13:21:36 -0800

    prototype sprites

--- prototype/main.js
+++ prototype/main.js
@@ -19,11 +19,6 @@ imgElement.onload = function() {
          *   - net.segmentMultiPersonParts
          * See documentation below for details on each method.
          */
-
-        let resized = new cv.Mat();
-
-        const w =44; const h = 47;
-        let dsize = new cv.Size(w, h);
         const segmentation = await net.segmentPerson(img);
         const foregroundColor = {r: 0, g: 0, b: 0, a: 0};
         const backgroundColor = {r: 255, g: 255, b:255, a: 255};
@@ -40,90 +35,127 @@ imgElement.onload = function() {
         bodyPix.drawMask(nobg, img, backgroundDarkeningMask, opacity, maskBlurAmount,  flipHorizontal);  
         bodyPix.drawMask(mask, img, backgroundDarkeningMask2, opacity, maskBlurAmount,  flipHorizontal);
 
-        let nobgimg =  cv.imread('nobg');
-        let src = cv.imread('mask');
-        let rectangular_image = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-        let dst = new cv.Mat();resized = new cv.Mat();
-
-
-        cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-        cv.threshold(src, src, 177, 200, cv.THRESH_BINARY);
-        let contours = new cv.MatVector();
-        let hierarchy = new cv.Mat();
-        cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
-        let cnt = contours.get(0);
-        let rect = cv.boundingRect(cnt);
-        rectangular_image = nobgimg.roi(rect);
-        mask_cut = src.roi(rect);
-
-        dsize = new cv.Size(w, h);
-        cv.resize(mask_cut, mask_cut, dsize, 0, 0, cv.INTER_AREA);            
-        cv.imshow('mask',mask_cut);
-        cv.resize(rectangular_image, resized, dsize, 0, 0, cv.INTER_AREA);
-        cv.imshow('rect',resized);
-
-        //Swap colors
-        let rgbaPlanes = new cv.MatVector();
-        let mergedPlanes = new cv.MatVector();
-        // Split the src
-        cv.split(resized, rgbaPlanes);
-        // Get G channel
-        let R = rgbaPlanes.get(0);
-
-        let G = rgbaPlanes.get(1);
-        // Get B channel
-        let B = rgbaPlanes.get(2);
-        // Merge G & B channels
-        mergedPlanes.push_back(G);
-        mergedPlanes.push_back(G);
-        mergedPlanes.push_back(G);
-        cv.merge(mergedPlanes, dst);
-        cv.imshow('swapcolors', dst);
-
-
-        
-        dsize = new cv.Size(rectangular_image.cols, rectangular_image.rows);
-        let center = new cv.Point(rectangular_image.cols / 2, rectangular_image.rows / 2);
-        let M = cv.getRotationMatrix2D(center, -3, 1);
-        cv.warpAffine(rectangular_image, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
-
-        dsize = new cv.Size(w, h);
-        cv.resize(dst, resized, dsize, 0, 0, cv.INTER_AREA);
-        cv.imshow('rect1', resized);
-
-        //Warp mask1
-        dsize = new cv.Size(mask_cut.cols, mask_cut.rows);
-        center = new cv.Point(mask_cut.cols / 2, mask_cut.rows / 2);
-        M = cv.getRotationMatrix2D(center, -3, 1);
-        cv.warpAffine(mask_cut, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
-        cv.imshow('mask1', dst);   
-
-        //Warp mask2
-        dsize = new cv.Size(mask_cut.cols, mask_cut.rows);
-        center = new cv.Point(mask_cut.cols / 2, mask_cut.rows / 2);
-        M = cv.getRotationMatrix2D(center, 3, 1);
-        cv.warpAffine(mask_cut, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
-        cv.imshow('mask2', dst);   
-        
-        M = cv.getRotationMatrix2D(center, 3, 1);
-        dsize = new cv.Size(rectangular_image.cols, rectangular_image.rows);
-        cv.warpAffine(rectangular_image, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
-        dsize = new cv.Size(w, h);
-        cv.resize(dst, resized, dsize, 0, 0, cv.INTER_AREA);
-        cv.imshow('rect2', resized);
-
-
-        M.delete(); dst.delete();  rectangular_image.delete(); src.delete(); contours.delete(); hierarchy.delete(); cnt.delete();
+        await resize_imgs();
         await delete_images();
     }
 
+    async function resize_imgs(){
+
+      let mask_mat = new cv.Mat(); let mask_mat1 = new cv.Mat(); let mask_mat2 = new cv.Mat();
+      let frame = new cv.Mat(); let frame1 = new cv.Mat(); let frame2 = new cv.Mat(); let bg = new cv.Mat();
+      let rgbaPlanes = new cv.MatVector();
+      let mergedPlanes = new cv.MatVector();
+
+      const w =44; const h = 47;
+
+      //let resized = new cv.Mat();
+      let dsize = new cv.Size(w, h);
+     
+
+      let nobgimg =  cv.imread('nobg');
+      let src = cv.imread('mask');
+      let rectangular_image = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+      cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+      cv.threshold(src, src, 177, 200, cv.THRESH_BINARY);
+      let contours = new cv.MatVector();
+      let hierarchy = new cv.Mat();
+      cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+      let cnt = contours.get(0);
+      let rect = cv.boundingRect(cnt);
+      frame = nobgimg.roi(rect);
+      mask_mat = src.roi(rect);
+
+
+      dsize = new cv.Size(w, h);
+      cv.resize(mask_mat, mask_mat, dsize, 0, 0, cv.INTER_AREA);            
+      cv.threshold(mask_mat, mask_mat, 20, 255, cv.THRESH_BINARY);
+      cv.imshow('mask',mask_mat);
+      cv.split(mask_mat, rgbaPlanes);
+      let MASKU = rgbaPlanes.get(0);      
+     
+
+      
+
+      cv.resize(frame, frame, dsize, 0, 0, cv.INTER_AREA);
+
+
+
+      cv.split(frame, rgbaPlanes);
+      let channel = rgbaPlanes.get(0);
+      mergedPlanes.push_back(channel);
+      channel = rgbaPlanes.get(1);
+      mergedPlanes.push_back(channel);
+      channel = rgbaPlanes.get(2);
+      mergedPlanes.push_back(channel);
+      mergedPlanes.push_back(MASKU);
+
+
+      cv.merge(mergedPlanes, frame);
+      cv.imshow('rect',frame);
+
+
+
+
+
+      // Split the src
+      mergedPlanes = new cv.MatVector();
+      cv.split(frame, rgbaPlanes);
+      // Get G channel
+      channel = rgbaPlanes.get(0);
+      mergedPlanes.push_back(channel);
+      mergedPlanes.push_back(channel);
+      mergedPlanes.push_back(channel);
+      mergedPlanes.push_back(MASKU);
+      cv.merge(mergedPlanes, bg);
+      cv.imshow('swapcolors', bg);
+
+
+      
+      dsize = new cv.Size(frame.cols, frame.rows);
+      let center = new cv.Point(frame.cols / 2, frame.rows / 2);
+      let M = cv.getRotationMatrix2D(center, -3, 1);
+      cv.warpAffine(frame, frame1, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+
+      /*dsize = new cv.Size(w, h);
+      cv.resize(dst, resized, dsize, 0, 0, cv.INTER_AREA);*/
+      cv.imshow('rect1', frame1);
+
+      //Warp mask1
+      dsize = new cv.Size(mask_mat.cols, mask_mat.rows);
+      center = new cv.Point(mask_mat.cols / 2, mask_mat.rows / 2);
+      M = cv.getRotationMatrix2D(center, -3, 1);
+      cv.warpAffine(mask_mat, mask_mat1, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+      cv.imshow('mask1', mask_mat1);   
+
+      //Warp mask2
+      dsize = new cv.Size(mask_mat.cols, mask_mat.rows);
+      center = new cv.Point(mask_mat.cols / 2, mask_mat.rows / 2);
+      M = cv.getRotationMatrix2D(center, 3, 1);
+      cv.warpAffine(mask_mat, mask_mat2, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+      cv.imshow('mask2', mask_mat2);   
+      
+      M = cv.getRotationMatrix2D(center, 3, 1);
+      dsize = new cv.Size(frame.cols, frame.rows);
+      cv.warpAffine(frame, frame2, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+      /*dsize = new cv.Size(w, h);
+      cv.resize(dst, resized, dsize, 0, 0, cv.INTER_AREA);*/
+      cv.imshow('rect2', frame2);
+
+
+      M.delete();  rectangular_image.delete(); src.delete(); contours.delete(); hierarchy.delete(); cnt.delete();
+    }
+
+
     async function delete_images(){
+
+        nobg.width = 1; nobg.height = 1;
+        img.src = "";
+        mask.width = 1; mask.height = 1;
+        let i =document.getElementById("mask1"); i.width=1; i.height=1;
+        i =document.getElementById("mask2"); i.width=1; i.height=1;
+
     /*let to_delete = document.getElementById("image");
         to_delete.parentElement.removeChild(to_delete);*/
-
-        /*to_delete = document.getElementById("nobg");
-        to_delete.parentElement.removeChild(to_delete);    */
-
 
     }
     execute();
